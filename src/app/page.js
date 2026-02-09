d41e5740a8d4d4b5548e4a67efc27819d9dd01e0
Mktvbr <mateusluizsr@hotmail.com>
2026-02-09 16:35:48 -0300

    feat(home): aprimora tipografia, layout visual e descrição da loja

--- src/app/page.js
+++ src/app/page.js
@@ -10,9 +10,17 @@ export default function Home() {
 
         <div className="flex flex-col items-center ">
 
-          <h1 className="text-6xl text-white mt-20 text-outline-soft leading-tight text-center"
-          >A Marmoraria Mais Conhecida da Região
+          <h1 className="text-4xl text-white mt-20 backdrop-blur-sm tracking-wide px-6 mb-1 rounded-full drop-shadow-[0_4px_20px_rgba(0,0,0,0.6)]text-outline-soft leading-tight text-center"
+          >O seu móvel em <span className="text-red-500">MÁRMORE</span>
           </h1>
+                    <h1 className="text-4xl text-white backdrop-blur-sm tracking-wide px-6 mb-4 rounded-full text-outline-soft leading-tight text-center"
+          >do jeito que você sempre sonhou
+          </h1>
+
+          <h2 className="mt-20">
+            <span className="text-2xl font-medium tracking-wider
+              text-white  backdrop-blur-sm rounded-full px-6 py-2 mt-4">Transformando casas em:</span>
+          </h2>
           <div className="mt-10 flex flex-row items-center justify-center gap-6 ">
             <h2 className="mt-2 px-6 py-2 text-2xl font-medium tracking-wider
               text-white bg-red-500/40 backdrop-blur-sm rounded-full">Coração de Maria</h2>
@@ -30,6 +38,9 @@ export default function Home() {
           priority
           className="object-cover -z-10 opacity-30"
         />
+      </main>
+      <main>
+        
       </main>
 
     </div>
